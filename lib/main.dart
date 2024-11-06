df0b353459cea1529559f3446bb80f31187122e1
Zienararya <kenzienararya981@gmail.com>
2024-11-06 14:17:20 +0700

    panggil data saat aplikasi dimulai

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,42 @@
+import 'package:dompetkos/helpers/db_instance.dart';
+import 'package:dompetkos/models/kategori.dart';
 import 'package:flutter/material.dart';
 import 'page/home.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DatabaseInstance databaseInstance = DatabaseInstance();
+
+  @override
+  void initState() {
+    databaseInstance.database();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'DompetKos', home: Homepage());
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: FutureBuilder<List<CategoryModel>>(
+            future: databaseInstance.all(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return MaterialApp(title: 'DompetKos', home: Homepage());
+              } else {
+                return Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.blue,
+                  ),
+                );
+              }
+            }));
   }
 }
- 
